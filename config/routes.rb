5f10a6d0f6cff227ddb2bf657fd7314a2df1f257
Talento <innovacion@talentocorporativo.com>
2010-04-12 23:06:53 +0200

    added devise configuration

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,12 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "welcome", :pages => "show"
+  map.root :controller => "pages", :pages => "show"
+
+
+  # devise user definition
+  map.devise_for :users
+  map.resource :user, :only => [:new, :create, :edit, :update]
 
   # See how all your routes lay out with "rake routes"
 
