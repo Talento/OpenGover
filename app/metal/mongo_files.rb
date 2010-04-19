60e8dedb9ed46e54cc372b95a24138e698080f4c
Talento <innovacion@talentocorporativo.com>
2010-04-19 16:06:17 +0200

    update gridFS version

--- app/metal/mongo_files.rb
+++ app/metal/mongo_files.rb
@@ -3,12 +3,12 @@ require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Ra
 require(File.dirname(__FILE__) + "/../../config/initializers/mongomapper") unless defined?(Rails)
 
 require 'mongo'
-require 'mongo/gridfs'
+#require 'mongo/gridfs'
 require "rmagick"
 require 'fileutils'
 
 include Mongo
-include GridFS
+#include GridFS
 
 class MongoFiles
   def self.call(env)
@@ -22,38 +22,56 @@ class MongoFiles
         key =   $4
       }
       crop = true if env["PATH_INFO"] =~ /^\/uploads\/images\/c(.+)$/
-      if GridStore.exist?(MongoMapper.database,key)
-          GridStore.open(MongoMapper.database, key, 'r') do |file|
+      begin
+        file = Mongo::Grid.new(MongoMapper.database).get(Mongo::ObjectID.from_string(key))
+
+     # if GridStore.exist?(MongoMapper.database,key)
+     #     GridStore.open(MongoMapper.database, key, 'r') do |file|
 
 
-    img = ::Magick::Image.from_blob(file.read).first
-    resized = img.crop_resized(width,height)
+#    img = ::Magick::Image.from_blob(file.read).first
+#    resized = img.crop_resized(width,height)
+        resized = file
                                                     #resized = nil
-    #ImageScience.with_image_from_memory(file.read) do |img_file|
-    #   resized = img_file.resize(width.to_i,height.to_i) do |f|
-        #FileUtils.mkdir_p(Rails.public_path + env["PATH_INFO"])
-        #f.save(Rails.public_path + env["PATH_INFO"])
-    #  end
-    #end
-    #resized_image = File.open(Rails.public_path + env["PATH_INFO"],'r')
-    #[200, {"Content-Type" => MIME::Types.type_for(resized_image.path).first.content_type}, [resized_image.read]]
+    ##ImageScience.with_image_from_memory(file.read) do |img_file|
+    ##   resized = img_file.resize(width.to_i,height.to_i) do |f|
+        ##FileUtils.mkdir_p(Rails.public_path + env["PATH_INFO"])
+        ##f.save(Rails.public_path + env["PATH_INFO"])
+    ##  end
+    ##end
+    ##resized_image = File.open(Rails.public_path + env["PATH_INFO"],'r')
+    ##[200, {"Content-Type" => MIME::Types.type_for(resized_image.path).first.content_type}, [resized_image.read]]
 
               [200, {"Content-Type" => file.content_type}, [resized.read]]
-            end
-      else
+            #end
+      rescue
         [404, {"Content-Type" => "text/html"}, ["Not Found"]]
         end
     elsif env["PATH_INFO"] =~ /^\/uploads\/files\/(.+)$/
       key="images/#{$1}"
-      if GridStore.exist?(MongoMapper.database,key)
-          GridStore.open(MongoMapper.database, key, 'r') do |file|
+      begin
+        file = Mongo::Grid.new(MongoMapper.database).get(Mongo::ObjectID.from_string(key))
+      #if GridStore.exist?(MongoMapper.database,key)
+      #    GridStore.open(MongoMapper.database, key, 'r') do |file|
               [200, {"Content-Type" => file.content_type}, [file.read]]
-            end
-      else
+      #      end
+      #else
+      rescue
         [404, {"Content-Type" => "text/html"}, ["Not Found"]]
         end
     else
       [404, {"Content-Type" => "text/html"}, ["Not Found"]]
     end
   end
+
+
+    private
+    def connect!
+      Timeout::timeout(5) do
+        @db = Mongo::Connection.new(hostname).db(database)
+      end
+    rescue StandardError => e
+      raise ConnectionError, "Timeout connecting to GridFS (#{e.to_s})"
+    end
+  
 end
