7729338f7885db3a930b448927f6206aee8d256b
Talento <innovacion@talentocorporativo.com>
2010-04-23 12:15:54 +0200

    added og_permission_helper

---
+++ vendor/plugins/og_security/lib/og_permission_helper.rb
@@ -0,0 +1,10 @@
+#
+#module OgPermissionHelper
+#    def after_sign_in_path_for(resource)
+#      {:controller => 'pages', :action => 'show', :og_user_login => current_user.id}
+#    end
+#end
+#
+#ApplicationController.send(:include, OgPermissionHelper)
+#
+#
