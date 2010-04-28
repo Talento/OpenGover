a1757e3f1f4842e9e33b30f262b8c138ace7c0dc
Iñaki Quesada Inclán <iquesada@iquesada-mac.local>
2010-04-28 18:59:23 +0200

    Bug fixed

--- vendor/plugins/og_seo_optimizer/lib/og_base_seo_optimizer_controller.rb
+++ vendor/plugins/og_seo_optimizer/lib/og_base_seo_optimizer_controller.rb
@@ -17,10 +17,9 @@ module OgBaseSeoOptimizerController
 
     def og_metas
       if response.content_type.to_s=="text/html" && !params[:og_template].blank? && !request.xhr?
-        action_hash = ActionController::Routing::Routes.recognize_path(request.request_uri , :method => :get)
         metas = OgSeoOptimizer.og_metas
         if metas["site_#{params[:og_site].id}"] && metas["site_#{params[:og_site].id}"][params[:og_locale]] && metas["site_#{params[:og_site].id}"][params[:og_locale]]["enabled"]=="1"
-          p = [action_hash[:controller], action_hash[:action],action_hash[:id]].compact.join("/")
+          p = [controller_name, action_name,params[:id]].compact.join("/")
           p="(blank)" if p.blank? || p=="pages/index"
           @meta = metas["site_#{params[:og_site].id}"][params[:og_locale]][p]
           unless @meta.blank?
@@ -34,10 +33,9 @@ module OgBaseSeoOptimizerController
 
     def og_conversions
       if response.content_type.to_s=="text/html" && !params[:og_template].blank? && !request.xhr?
-        action_hash = ActionController::Routing::Routes.recognize_path(request.request_uri , :method => :get)
         conversions = OgSeoOptimizer.og_conversions
         if conversions["site_#{params[:og_site].id}"] && conversions["site_#{params[:og_site].id}"][params[:og_locale]] && conversions["site_#{params[:og_site].id}"][params[:og_locale]]["enabled"]=="1"
-          p = [action_hash[:controller], action_hash[:action],action_hash[:id]].compact.join("/")
+          p = [controller_name, action_name,params[:id]].compact.join("/")
           p="(blank)" if p.blank? || p=="pages/index"
           @conversion = conversions["site_#{params[:og_site].id}"][params[:og_locale]][p]
           unless @conversion.blank?
